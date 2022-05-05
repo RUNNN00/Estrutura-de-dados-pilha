092658966f783fc73233656fc225f4847b4d1399
RUAN CARLOS DE SOUZA AZEVEDO <a2254131@debian.dacom>
2022-05-05 17:35:39 -0300

    tentando criar primeiras funcoes

--- estrutura-de-dados/main.c
+++ estrutura-de-dados/main.c
@@ -3,9 +3,6 @@
 int main()
 {
     Pilha* pilha = pilha_criar();
-    printf("%u\n", pilha);
-    pilha = NULL;
-    printf("%u\n", pilha);
 
     /*pilha_empilhar(pilha, 10);
     pilha_empilhar(pilha, 20);
--- estrutura-de-dados/pilha_encadeada.c
+++ estrutura-de-dados/pilha_encadeada.c
@@ -4,20 +4,34 @@ typedef struct no
 {
     TipoElemento dado;
     struct no *prox;
-}No;
+} No;
+
 
 struct pilha
 {
-    No* topo;
+    No *topo;
     int qtdeElemento;
 };
 
 
-Pilha* pilha_criar()
+Pilha *pilha_criar()
 {
-    Pilha* p = (Pilha*) malloc(sizeof(Pilha));
+    Pilha *p = (Pilha *)malloc(sizeof(Pilha));
     p->qtdeElemento = 0;
     p->topo = NULL;
 
     return p;
+}
+
+
+void pilha_destruir(Pilha **endereco)
+{
+}
+
+bool pilha_empilhar(Pilha* p, TipoElemento elemento)
+{
+    No* no = (No*) malloc(sizeof(No));
+    no->dado = elemento;
+    no->prox = NULL;
+    
 }
