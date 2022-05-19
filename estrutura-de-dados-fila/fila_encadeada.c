05455a75d4e0a8353418a1f5b110f6b8492277a8
RUAN CARLOS DE SOUZA AZEVEDO <a2254131@debian.dacom>
2022-05-19 17:25:06 -0300

    Preparando arquivos para implementar a Fila

---
+++ estrutura-de-dados-fila/fila_encadeada.c
@@ -0,0 +1,43 @@
+#include "fila.h"
+
+/**************************************
+ * DADOS
+ **************************************/
+typedef struct no
+{
+    int dado;
+    struct no *prox;
+} No;
+
+struct fila
+{
+    No *inicio;
+    No *fim;
+    int qtde;
+};
+
+/**************************************
+ * IMPLEMENTAÇÃO
+ **************************************/
+Fila *fila_criar()
+{
+    Fila* f = (Fila*) malloc(sizeof(Fila));
+    f->qtde = 0;
+    f->fim = NULL;
+    f->inicio = NULL;
+}
+
+void fila_destruir(Fila **enderecoFila);
+bool fila_inserir(Fila *f, TipoElemento elemento)
+{
+    //TODO
+}
+
+bool fila_remover(Fila *f, TipoElemento *saida);  // estratégia do scanf
+bool fila_primeiro(Fila *f, TipoElemento *saida); // estratégia do scanf
+bool fila_vazia(Fila *f);
+int fila_tamanho(Fila *f);
+void fila_imprimir(Fila *f);
+Fila *fila_clone(Fila *f);
+bool fila_toString(Fila *f, char *str);
+bool fila_inserirTodos(Fila *f, TipoElemento *vetor, int tamVetor);
