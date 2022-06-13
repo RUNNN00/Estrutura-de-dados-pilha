a49262f1d8a096d2c98eef2b3f7147210671f2f5
RUAN CARLOS DE SOUZA AZEVEDO <a2254131@debian.dacom>
2022-06-13 13:50:37 -0300

    Implementando o redstante das funcoes - falta testar

--- lista/lista_encadeada_sentinela.c
+++ lista/lista_encadeada_sentinela.c
@@ -15,6 +15,32 @@ struct no
     No *prox;
 };
 
+// AUXILIARES
+No *localizaNo(Lista *l, int pos)
+{
+    if (pos > l->qtd * 0.5)
+    {
+        No *aux = l->sentinela->ant;
+        for (int i = l->qtd - 1; i >= 0; i--)
+        {
+            if (i == pos)
+                return aux;
+            aux = aux->ant;
+        }
+    }
+    else
+    {
+        No *aux = l->sentinela->prox;
+        for (int i = 0; i < l->qtd; i++)
+        {
+            if (i == pos)
+                return aux;
+            aux = aux->prox;
+        }
+    }
+    return NULL;
+}
+
 // IMPLEMENTAÇÃO
 Lista *lista_criar()
 {
@@ -67,14 +93,126 @@ bool lista_anexar(Lista *l, TipoElemento elemento)
     return true;
 }
 
-bool lista_inserir(Lista *l, int posicao, TipoElemento elemento);
-bool lista_removerPosicao(Lista *l, int *saida, int posicao);
-int lista_removerElemento(Lista *l, TipoElemento elemento);
+bool lista_inserir(Lista *l, int posicao, TipoElemento elemento)
+{
+    if (l == NULL)
+        return false;
+    if (posicao > l->qtd || posicao < 0)
+        return false;
+    if (posicao == l->qtd)
+    {
+        lista_anexar(l, elemento);
+        return true;
+    }
+
+    No *new = (No *)malloc(sizeof(No));
+
+    if (new == NULL)
+        return false;
+
+    new->dado = elemento;
+
+    // ajuste nos posicionamentos dos nos
+    No *selec = localizaNo(l, posicao);
+    if (selec == NULL)
+        return false;
+    selec->ant->prox = new;
+    new->ant = selec->ant;
+    new->prox = selec;
+    selec->ant = new;
+
+    l->qtd++;
+    return true;
+}
+
+bool lista_removerPosicao(Lista *l, int *saida, int posicao)
+{
+    if (l == NULL)
+        return false;
+    if (posicao > l->qtd || posicao < 0)
+        return false;
+
+    No *selec = localizaNo(l, posicao);
+    if (selec == NULL)
+        return false;
+    *saida = selec->dado;
+
+    // ajuste nos posicionamentos dos nos
+    selec->ant->prox = selec->prox;
+    selec->prox->ant = selec->ant;
+    free(selec);
+
+    l->qtd--;
+    return true;
+}
+
+int lista_removerElemento(Lista *l, TipoElemento elemento)
+{
+    if (l == NULL)
+        return -1;
+
+    int index = 0;
+    No *selec = NULL;
+    No *aux = l->sentinela->prox;
+    for (int i = 0; i < l->qtd; i++)
+    {
+        if (elemento == aux->dado)
+        {
+            selec = aux;
+            index = i;
+        }
+        aux = aux->prox;
+    }
+
+    if (selec == NULL)
+        return -1;
+
+    // ajuste nos posicionamentos dos nos
+    selec->ant->prox = selec->prox;
+    selec->prox->ant = selec->ant;
+    free(selec);
+
+    l->qtd--;
+    return index;
+}
+
 bool lista_substituir(Lista *l, int posicao, TipoElemento elemento);
-int lista_posicao(Lista *l, TipoElemento elemento);
-bool lista_buscar(Lista *l, int posicao, TipoElemento *saida);
-int lista_tamanho(Lista *l);
-bool lista_vazia(Lista *l);
+int lista_posicao(Lista *l, TipoElemento elemento)
+{
+    No *aux = l->sentinela->prox;
+    for (int i = 0; i < l->qtd; i++)
+    {
+        if (elemento == aux->dado)
+            return i;
+        aux = aux->prox;
+    }
+    return -1;
+}
+
+bool lista_buscar(Lista *l, int posicao, TipoElemento *saida)
+{
+    if (l == NULL)
+        return false;
+    if (posicao > l->qtd || posicao < 0)
+        return false;
+
+    No *selec = localizaNo(l, posicao);
+    if (selec == NULL)
+        return false;
+    *saida = selec->dado;
+    return true;
+}
+
+int lista_tamanho(Lista *l)
+{
+    return l->qtd;
+}
+
+bool lista_vazia(Lista *l)
+{
+    return l->qtd <= 0;
+}
+
 bool lista_toString(Lista *l, char str[])
 {
     str[0] = '\0';
--- lista/main.c
+++ lista/main.c
@@ -6,27 +6,26 @@ int main()
     char str[100];
     int posicao = 0;
     int elemento = 0;
+    int elementoBusca = 0;
 
     lista_anexar(l1, 10);
     lista_anexar(l1, 20);
     lista_anexar(l1, 30);
     lista_anexar(l1, 40);
-    //lista_inserir(l1, 2, -5);
+    lista_inserir(l1, 2, -5);
     lista_anexar(l1, 50);
-    /*lista_inserir(l1, 0, 5);
-    lista_inserir(l1, 7, -60);
-    lista_inserir(l1, 5, -70);
-    lista_removerPosicao(l1, &posicao, 5);
-    lista_removerPosicao(l1, &posicao, 3);
+    lista_removerPosicao(l1, &elemento, 3);
     posicao = lista_removerElemento(l1, 10);
-    lista_substituir(l1, 0, -100);
-    posicao = lista_posicao(l1, 40);
-    lista_buscar(l1, 2, &elemento);*/
+    /*lista_substituir(l1, 0, -100);
+    posicao = lista_posicao(l1, 40);*/
+    lista_buscar(l1, 2, &elementoBusca);
+    printf("Elemento buscado: %d\n", elementoBusca);
 
     lista_toString(l1, str);
     printf("%s\n", str);
-    /*printf("\n%s\nposicao do posicao 40: %d\n", str, posicao);
-    printf("elemento buscado: %d\n", elemento);*/
+    //printf("\n%s\nposicao do posicao 40: %d\n", str, posicao);
+    printf("elemento removido: %d\n", elemento);
+    printf("posicao removido: %d\n", posicao);
 
     return 0;
 }
