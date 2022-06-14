83ee69c087bb38d84980c71f9ec16e8323db9a06
ruanazevedo <ruann.azevedo@outlook.com>
2022-06-14 15:30:25 -0300

    Implementando a lista contigua completa e testada

--- lista/main.c
+++ lista/main.c
@@ -11,22 +11,38 @@ int main()
     lista_anexar(l1, 20);
     lista_anexar(l1, 30);
     lista_anexar(l1, 40);
-    //lista_inserir(l1, 2, -5);
     lista_anexar(l1, 50);
-    /*lista_inserir(l1, 0, 5);
-    lista_inserir(l1, 7, -60);
-    lista_inserir(l1, 5, -70);
-    lista_removerPosicao(l1, &posicao, 5);
-    lista_removerPosicao(l1, &posicao, 3);
-    posicao = lista_removerElemento(l1, 10);
-    lista_substituir(l1, 0, -100);
-    posicao = lista_posicao(l1, 40);
-    lista_buscar(l1, 2, &elemento);*/
-
+    lista_anexar(l1, 60);
+    lista_anexar(l1, 70);
     lista_toString(l1, str);
-    printf("%s\n", str);
-    /*printf("\n%s\nposicao do posicao 40: %d\n", str, posicao);
-    printf("elemento buscado: %d\n", elemento);*/
+    printf("%s\n\n", str);
 
+    printf("inserir\n");
+    lista_inserir(l1, 1, -5);
+    lista_toString(l1, str);
+    printf("%s\n\n", str);
+
+
+    lista_removerPosicao(l1, &elemento, 5);
+    printf("remover posicao: %d\n", elemento);
+    lista_toString(l1, str);
+    printf("%s\n\n", str);
+
+    posicao = lista_removerElemento(l1, 10);
+    printf("remover elemento - posicao: %d\n", posicao);
+    lista_toString(l1, str);
+    printf("%s\n\n", str);
+
+    printf("substituir\n");
+    lista_substituir(l1, 3, -100);
+    lista_toString(l1, str);
+    printf("%s\n\n", str);
+
+    posicao = lista_posicao(l1, 40);
+    printf("posicao do elemento 40: %d\n\n", posicao);
+
+    lista_buscar(l1, 2, &elemento);
+    printf("buscar posicao 2: %d\n\n", elemento);
+    
     return 0;
 }
